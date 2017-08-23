18b7285bfda709ddefe59d4f54411d9434d041a7
Koushik Dutta <koushd@gmail.com>
2017-08-23 16:13:18 +0200

    envsetup: Add roomservice
    Partial squash of:
    
      Author: Luca Stefani <luca.stefani.ge1@gmail.com>
      Date: Sun, 1 Jan 2017 21:45:07 +0100
    
        lunch: Use cd - instead of popd
    
        * ZSH breaks a lot of things.
    
        Change-Id: Idee8485bee673bebf6fe5857253306b20aa92eb5
    
      Author: Luca Stefani <luca.stefani.ge1@gmail.com>
      Date: Thu, 5 Jan 2017 16:30:15 +0100
    
        build: Replace pushd with cd
    
        * For consistency
    
        Change-Id: I96711e3be7b73ebadd589f460e5c5e90d000a4f7
    
      Author: Koushik Dutta <koushd@gmail.com>
      Date: Sat, 26 Nov 2011 18:51:42 -0800
    
        roomservice delivers you lunch combos from the CyanogenMod github.
    
        fix roomservice formatting
        support product names with _ in them
        fix roomservice to handle pagination
    
        Change-Id: I4923c2f768094dbad8e06a72d9f27d46414030ab
    
      Author: LuK1337 <priv.luk@gmail.com>
      Date:   Tue Apr 13 19:57:24 2021 +0200
    
        envsetup: Fix the way we check check_product's return value
    
        Change-Id: I5a2a4b7be5dfee198094902aaa2c9a8c27b97704
    
    Change-Id: I5bb07944f558e3e5551ab65c97ecfa303609be57

--- envsetup.sh
+++ envsetup.sh
@@ -452,6 +452,15 @@ function _lunch_meat()
         then
             echo "Did you mean -${product/*_/}? (dash instead of underscore)"
         fi
+        echo
+        echo "** Don't have a product spec for: '$product'"
+        echo "** Do you have the right repo manifest?"
+        product=
+    fi
+
+    if [ -z "$product" -o -z "$variant" ]
+    then
+        echo
         return 1
     fi
     export TARGET_PRODUCT=$(_get_build_var_cached TARGET_PRODUCT)
@@ -564,6 +573,21 @@ function lunch()
         if [[ -z $variant ]]; then
             variant=eng
         fi
+    fi
+
+    if ! check_product $product $release
+    then
+        # if we can't find a product, try to grab it off the auroraOSP-devices GitHub
+        T=$(gettop)
+        cd $T > /dev/null
+        vendor/aurora/build/tools/roomservice.py $product
+        cd - > /dev/null
+        check_product $product $release
+    else
+        T=$(gettop)
+        cd $T > /dev/null
+        vendor/aurora/build/tools/roomservice.py $product true
+        cd - > /dev/null
     fi
 
     # Validate the selection and set all the environment stuff
