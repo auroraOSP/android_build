ed656f864a7a26e2307f928af2fa75743e40ea2d
Alvin Francis <nivlafx@gmail.com>
2024-08-10 12:07:16 -0400

    Continue module SDK building when platform sdk fails.
    Change-Id: I55360e79ae2f8afdc8487ddc64b143d22e7516ca
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- tools/finalization/localonly-steps.sh
+++ tools/finalization/localonly-steps.sh
@@ -21,14 +21,15 @@ function finalize_locally() {
     # $m adb
     # LOGNAME=android-eng HOSTNAME=google.com "$top/out/host/linux-x86/bin/adb" keygen "$top/vendor/google/security/adb/${FINAL_PLATFORM_VERSION}.adb_key"
 
-    # Build Platform SDKs.
-    # $top/build/soong/soong_ui.bash --make-mode TARGET_PRODUCT=sdk TARGET_RELEASE=ap2a TARGET_BUILD_VARIANT=user sdk dist sdk_repo DIST_DIR=out/dist
-
     # Build Modules SDKs.
+    echo "Building Modules SDKs..."
     TARGET_RELEASE=ap4a TARGET_BUILD_VARIANT=userdebug UNBUNDLED_BUILD_SDKS_FROM_SOURCE=true DIST_DIR=out/dist "$top/packages/modules/common/build/mainline_modules_sdks.sh" --build-release=$module_build_release_ver
 
     # Update prebuilts.
-    "$top/prebuilts/build-tools/path/linux-x86/python3" -W ignore::DeprecationWarning "$top/prebuilts/sdk/update_prebuilts.py" --local_mode -f ${FINAL_PLATFORM_SDK_VERSION} -e ${FINAL_MAINLINE_EXTENSION} --bug 1 1
+    echo "Updating module SDK prebuilts..."
+    "$top/prebuilts/build-tools/path/linux-x86/python3" -W ignore::DeprecationWarning "$top/prebuilts/sdk/update_prebuilts.py" --local_mode -f ${FINAL_PLATFORM_SDK_VERSION} -e ${FINAL_MAINLINE_EXTENSION} --bug 1 1 || {
+        echo "Platform SDK update failed, but continuing..."
+    }
 }
 
 function copy_sdk_files() {
