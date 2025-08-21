7e3dcfc9ac9cb4ac06e9f567c5382dd4c266c2d7
Nolen Johnson <johnsonnolen@gmail.com>
2025-08-21 19:22:09 -0400

    gen_build-manifest: Ensure that we in TOP when executed
    Change-Id: I687afeee3f820412a565291ced7d100bc23ce857

--- envsetup.sh
+++ envsetup.sh
@@ -621,7 +621,9 @@ function lunch()
     fi
 
     # Generate build-manifest.xml after roomservice finishes
+    cd $T > /dev/null
     vendor/aurora/build/tools/gen_build-manifest.sh
+    cd - > /dev/null
 
     # Validate the selection and set all the environment stuff
     _lunch_meat $product $release $variant
